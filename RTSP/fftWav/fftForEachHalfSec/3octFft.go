358907b079151f1cf376a749fbb47531a46d8600
motoakioi <oi@comp.sd.keio.ac.jp>
2019-01-14 21:12:21 +0900

    [[divide] main file]

--- RTSP/fftWav/fftForEachHalfSec/3octFft.go
+++ RTSP/fftWav/fftForEachHalfSec/3octFft.go
@@ -2,139 +2,15 @@ package main
 
 import (
 	"fmt"
-	"image/color"
 	"log"
 	"math"
 	"math/cmplx"
-	"os"
 
-	"./figHandle"
+	"github.com/motoakioi/Go-for-pastime/RTSP/fftWav/fftForEachHalfSec/figHandle"
+	"github.com/motoakioi/Go-for-pastime/RTSP/fftWav/fftForEachHalfSec/readWav"
 
-	"github.com/oov/audio/wave"
 	"github.com/r9y9/go-dsp/fft"
-	"gonum.org/v1/plot"
-	"gonum.org/v1/plot/plotter"
-	"gonum.org/v1/plot/vg"
 )
-
-// Show WAV data format
-func FmtDisplay(wfe *wave.WaveFormatExtensible) {
-
-	fmt.Println("...")
-	fmt.Println(" Samplerate:", wfe.Format.SamplesPerSec)
-	fmt.Println(" Channels  :", wfe.Format.Channels)
-	fmt.Println(" Bits      :", wfe.Format.BitsPerSample)
-
-}
-
-// Get wav data from file
-func getWavData(fileName string) ([][]float64, *wave.WaveFormatExtensible) {
-
-	// File open and close
-	file, erFile := os.Open(fileName)
-	defer file.Close()
-	// In case of error
-	if erFile != nil {
-		log.Fatal("Can NOT open .wav file.")
-	}
-
-	// File Size
-	fInfo, erFInfo := file.Stat()
-	// In case of error
-	if erFInfo != nil {
-		log.Fatal("Can NOT get file information.")
-	}
-	fmt.Println("File size is ", fInfo.Size())
-
-	// Read data from .wav file
-	data, wfe, erData := wave.NewReader(file)
-	// In case of error
-	if erData != nil {
-		log.Fatal("Can NOT read .wav data.")
-	}
-
-	// Calculate duration
-	fHeaderSize := int64(44)
-	fSize := float32(fInfo.Size() - fHeaderSize)
-	wavCh := float32(wfe.Format.Channels)
-	wavBit := float32(wfe.Format.BitsPerSample)
-	numSample := int(fSize / (wavCh * (wavBit / 8.0)))
-	fmt.Println("Total sample points are ", numSample)
-
-	// Create buffer for data handle
-	inTmp := [][]float64{}
-	for i := 0; i < int(wavCh); i++ {
-		inTmp = append(inTmp, make([]float64, numSample))
-	}
-
-	// Read wave data from struct
-	_, erN := data.ReadFloat64Interleaved(inTmp)
-	if erN != nil {
-		log.Fatal("Can NOT read data from inTmp.")
-	}
-
-	return inTmp, wfe
-}
-
-// Create figure
-func cre8Figure() *plot.Plot {
-
-	fig, erFig := plot.New()
-	if erFig != nil {
-		log.Fatal("Can NOT create figure.")
-	}
-
-	return fig
-}
-
-type plotRange struct {
-	xStart float64
-	xEnd   float64
-	yStart float64
-	yEnd   float64
-}
-
-// Set figure
-func CfgFigure(fig *plot.Plot, figRange plotRange) {
-
-	// Label config
-	//fig.Title.Text = "CfgFigure func"
-	fig.X.Label.Text = "Frequency [Hz]"
-	//fig.Y.Label.Text = "y"
-
-	// Range for each axis
-	fig.X.Min = figRange.xStart
-	fig.X.Max = figRange.xEnd
-	fig.Y.Min = figRange.yStart
-	fig.Y.Max = figRange.yEnd
-}
-
-// Set plot struct
-func cfgPoint(x float64, dx float64, y []float64) plotter.XYs {
-	plotTmp := make(plotter.XYs, int(x/dx))
-	for i := 0; i < int(x/dx); i++ {
-		plotTmp[i].X = float64(i) * dx
-		plotTmp[i].Y = y[i]
-	}
-	return plotTmp
-}
-
-// Add line from data
-func addLine(fig *plot.Plot, xys plotter.XYs) {
-
-	// Create Line
-	plotLine, err := plotter.NewLine(xys)
-	if err != nil {
-		panic(err)
-	}
-	// Line config
-	plotLine.LineStyle.Width = vg.Points(1)
-	//plotLine.LineStyle.Dashes = []vg.Length{vg.Points(5), vg.Points(5)}
-	plotLine.LineStyle.Color = color.RGBA{B: 255, A: 255}
-
-	// Add line to figure
-	fig.Add(plotLine)
-}
 
 // Calculate power from complex number
 func c2power(inC []complex128) []float64 {
@@ -149,10 +25,10 @@ func c2power(inC []complex128) []float64 {
 func main() {
 
 	// Get wav data from file
-	wavData, wfe := getWavData("./3octaves.wav")
+	wavData, wfe := readWav.GetWavData("../wavData/3octaves.wav")
 
 	// Show wave data format
-	FmtDisplay(wfe)
+	readWav.FmtDisplay(wfe)
 
 	// fft
 	fftDataC := []complex128{}
@@ -162,22 +38,21 @@ func main() {
 	fftDataPow := c2power(fftDataC)
 
 	// Create figure
-	fig := cre8Figure()
+	fig := figHandle.Cre8Figure()
 
 	// Set range of plot
-	var figRange plotRange
-	figRange.xStart = 0
-	//figRange.xEnd = float64(len(fftDataPow) / 2)
-	figRange.xEnd = float64(wfe.Format.SamplesPerSec / 2)
-	figRange.yStart = 0
-	figRange.yEnd = 1500000
+	var figRange figHandle.PlotRange
+	figRange.XStart = 0
+	figRange.XEnd = float64(wfe.Format.SamplesPerSec / 2)
+	figRange.YStart = 0
+	figRange.YEnd = 1500000
 
 	// Set figure
-	CfgFigure(fig, figRange)
+	figHandle.CfgFigure(fig, figRange)
 
-	fmt.Println("x range after", figRange.xEnd)
+	fmt.Println("x range after", figRange.XEnd)
 	// Add data as line to figure
-	addLine(fig, cfgPoint(figRange.xEnd, 1.0, fftDataPow))
+	figHandle.AddLine(fig, figHandle.CfgPoint(figRange.XEnd, 1.0, fftDataPow))
 
 	/*
 		// Set function of plot
